badcb01efff8964ffb749e668a33998e2835f05f
Pamelloes <joshuabrot@gmail.com>
2011-12-31 12:00:37 -0600

    Set up the workspace.

---
+++ src/main/java/org/mcexchange/MinecraftExchange.java
@@ -0,0 +1,17 @@
+package org.mcexchange;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MinecraftExchange extends JavaPlugin {
+
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
