ea01c65989a06cf6bed4c00ddc3fd3859cd811cc
retsrif <retsrif@gmail.com>
2012-01-06 14:45:23 +0300

    setup connection with server

--- README.md
+++ README.md
@@ -1,10 +1,7 @@
 <img alt="Minecraft Exchange Logo" src="http://dl.dropbox.com/u/53010016/MCExchange.png" width="250px" />
 # Minecraft Exchange 
-The Minecraft Exchange is a program designed to emulate a real-life stock market in Minecraft. It lets users buy and
-sell items at any price with a recommended price given based off of supply and demand. The program has an "Economy
-Server" which has all of the economic data and an "Economy Client" which is the Minecraft Server. The MC Server would
-then submit its buy and sell offers to the Economy Server after a given interval where they would be completed. After
-all of the transactions that can be completed are finished, the Economy Server will then update the prices to reflect
-the new state of the economy.
+The Minecraft Exchange is a program used to connect multiple servers together through a middle man, which is the Minecraft Exchange server.
+This plugin is what allows for the interfacing with the server, which will allow the server using this plugin to access vast possibilities
+of systems such as economy systems and messaging systems.
 
 ## This is an Economy Client for Bukkit
--- src/main/java/org/mcexchange/MinecraftExchange.java
+++ src/main/java/org/mcexchange/MinecraftExchange.java
@@ -1,17 +1,64 @@
 package org.mcexchange;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MinecraftExchange extends JavaPlugin {
+	Socket socket = null;
+	PrintWriter out = null;
+	BufferedReader in = null;
 
 	public void onDisable() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("[Minecraft Exchange] is disconnecting from the server.");
+		out.close();
+		try {
+			in.close();
+		} catch (IOException e) {
+			System.out.println("[Minecraft Exchange] could not close input stream.");
+			e.printStackTrace();
+		}
+		try {
+			socket.close();
+		} catch (IOException e) {
+			System.out.println("[Minecraft Exchange] could not close connection.");
+			e.printStackTrace();
+		}
+		System.out.println("[Minecraft Exchange] disabled.");
 	}
 
 	public void onEnable() {
-		// TODO Auto-generated method stub
+		System.out.println("[Minecraft Exchange] is connecting to the server.");
 		
+		try{
+			//localhost just for testing purposes
+			socket = new Socket("localhost", 62924);
+			out = new PrintWriter(socket.getOutputStream(), true);
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		}
+		catch(UnknownHostException e) {
+			System.out.println("[Minecraft Exchange] could not connect to host.");
+			e.printStackTrace();
+			disable();
+		}
+		catch(IOException ioe) {
+			System.out.println("[Minecraft Exchange] could not get I/O for the connection.");
+			ioe.printStackTrace();
+			disable();
+		}
+		
+		System.out.println("[Minecraft Exchange] has connected to the server.");
+			
+		System.out.println("[Minecraft Exchange] enabled.");
+	}
+	
+	public void disable() {
+		this.setEnabled(false);
 	}
 
 }
