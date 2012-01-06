34e0e91dce17bb491607caf81e23f9710b31b402
Pamelloes <joshuabrot@gmail.com>
2012-01-06 15:52:28 -0600

    Expanded client.

--- src/main/java/org/mcexchange/MinecraftExchange.java
+++ src/main/java/org/mcexchange/MinecraftExchange.java
@@ -1,53 +1,38 @@
 package org.mcexchange;
 
-import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MinecraftExchange extends JavaPlugin {
-	Socket socket = null;
-	PrintWriter out = null;
-	BufferedReader in = null;
-
-	public void onDisable() {
-		System.out.println("[Minecraft Exchange] is disconnecting from the server.");
-		out.close();
-		try {
-			in.close();
-		} catch (IOException e) {
-			System.out.println("[Minecraft Exchange] could not close input stream.");
-			e.printStackTrace();
-		}
-		try {
-			socket.close();
-		} catch (IOException e) {
-			System.out.println("[Minecraft Exchange] could not close connection.");
-			e.printStackTrace();
-		}
-		System.out.println("[Minecraft Exchange] disabled.");
+	private static MinecraftExchange instance;
+	public static MinecraftExchange getInstance() { return instance; }
+	private ServerConnection sc;
+	private Thread sct;
+	
+	public void onLoad() {
+		instance = this;
 	}
 
 	public void onEnable() {
 		System.out.println("[Minecraft Exchange] is connecting to the server.");
-		
+		File f = new File(getDataFolder(), "config.yml");
+		if(!f.exists()) saveDefaultConfig();
 		try{
 			//localhost just for testing purposes
-			socket = new Socket("localhost", 62924);
-			out = new PrintWriter(socket.getOutputStream(), true);
-			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		}
-		catch(UnknownHostException e) {
-			System.out.println("[Minecraft Exchange] could not connect to host.");
+			Socket socket = new Socket(getConfig().getString("host", "localhost"), getConfig().getInt("port",62924));
+			sc = new ServerConnection(socket);
+			sct = new Thread(sc);
+			sct.start();
+		} catch(UnknownHostException e) {
+			System.err.println("[Minecraft Exchange] Could not connect to host.");
 			e.printStackTrace();
 			disable();
-		}
-		catch(IOException ioe) {
-			System.out.println("[Minecraft Exchange] could not get I/O for the connection.");
+		} catch(IOException ioe) {
+			System.err.println("[Minecraft Exchange] could not get I/O for the connection.");
 			ioe.printStackTrace();
 			disable();
 		}
@@ -55,6 +40,11 @@ public class MinecraftExchange extends JavaPlugin {
 		System.out.println("[Minecraft Exchange] has connected to the server.");
 			
 		System.out.println("[Minecraft Exchange] enabled.");
+	}
+
+	public void onDisable() {
+		sct.interrupt();
+		System.out.println("[Minecraft Exchange] disabled.");
 	}
 	
 	public void disable() {
