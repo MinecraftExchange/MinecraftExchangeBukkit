42b2a3101b78ec7859aea60eef018ae0e6778de9
Pamelloes <joshuabrot@gmail.com>
2012-01-08 16:10:17 -0600

    Perfecting packet system.

--- src/main/java/org/mcexchange/RegisteredPackets.java
+++ src/main/java/org/mcexchange/RegisteredPackets.java
@@ -6,13 +6,20 @@ package org.mcexchange;
  */
 public class RegisteredPackets {
 	private DisconnectPacket disconnect;
+	private MessagePacket message;
 	
 	public RegisteredPackets(ServerConnection sc) {
 		disconnect = new DisconnectPacket(sc);
 		Packet.assignId((byte)-127, disconnect);
+		message = new MessagePacket(sc);
+		Packet.assignId((byte)-126, disconnect);
 	}
 	
-	public Packet getDisconnect() {
+	public DisconnectPacket getDisconnect() {
 		return disconnect;
+	}
+	
+	public MessagePacket getMessage() {
+		return message;
 	}
 }
